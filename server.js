ba7a45e2face9c30fe706c69915ce5ae32cce19a
sithi1990 <sithi1990@live.com>
2016-07-05 10:58:44 +0530

    Changed auth api remote url

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ apiRoutes.use(function(req, res, next) {
 		headers: { "Content-Type": "application/json","x-access-token": token}
 	};
 	 
-	httpClient.get("http://localhost:8078/authapp/userinfo", args, function (userInfo, response) {
+	httpClient.get("http://vauthapp.herokuapp.com/authapp/userinfo", args, function (userInfo, response) {
 		
 	
 			if(userInfo.success)
